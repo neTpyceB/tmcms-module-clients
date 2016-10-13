04d94170fdf3848eb28cea51729cd310b77eafec
gamma <gamma@devp.eu>
2016-10-13 15:33:43 +0300

    added saveprofile and validate form fields methods

--- src/ModuleClients.php
+++ src/ModuleClients.php
@@ -7,6 +7,8 @@ use TMCms\Config\Configuration;
 use TMCms\Modules\Clients\Entity\ClientEntity;
 use TMCms\Modules\Clients\Entity\ClientEntityRepository;
 use TMCms\Modules\IModule;
+use TMCms\Modules\Sessions\ModuleSessions;
+use TMCms\Routing\Structure;
 use TMCms\Strings\UID;
 use TMCms\Traits\singletonInstanceTrait;
 
@@ -29,7 +31,9 @@ class ModuleClients implements IModule {
 		return Users::getInstance()->generateHash($password, self::$_password_salt . Configuration::getInstance()->get('cms')['unique_key']);
 	}
 
-	public static function register($post) {
+	private static function validateFields($post, $create_client = true) {
+
+		$result = [];
 		$errors = [];
 
 		// Check e-mail address (required)
@@ -37,30 +41,62 @@ class ModuleClients implements IModule {
 			$errors[] = w('Email_field_error');
 		}
 		else {
-			// Check existing user
+			// Check for existing user
 			$existing_client = ClientEntityRepository::findOneEntityByCriteria([
-				'email' => $post['email']
+					'email' => $post['email']
 			]);
-			if ($existing_client) {
-				$errors[] = w('Client_with_this_email_already_exist');
+
+			if ($create_client) {
+				if ($existing_client) {
+					$errors[] = w('Client_with_this_email_already_exist');
+				}
+			}
+			else {
+				if ($existing_client && $existing_client->getId() != $post['id']) {
+					$errors[] = w('Client_with_this_email_already_exist');
+				}
 			}
 		}
 
 		// Check password match (required)
-		if (empty($post['password']) || $post['password'] != $post['repeat_password']) {
-			$errors[] = w('Password_field_error_or_passwords_does_not_match');
+		if ($create_client) { // for registration
+			if (empty($post['password']) || $post['password'] != $post['repeat_password']) {
+				$errors[] = w('Password_field_error_or_passwords_does_not_match');
+			}
+			else {
+				$post['hash'] = self::generateHash($post['password']);
+			}
 		}
-		else {
-			$post['hash'] = self::generateHash($post['password']);
-			unset($post['password']);
-			unset($post['repeat_password']);
+		else { // for profile save
+			if (!empty($post['password'])) {
+				if ($post['password'] != $post['repeat_password']) {
+					$errors[] = w('Password_field_error_or_passwords_does_not_match');
+				}
+				else {
+					$post['hash'] = self::generateHash($post['password']);
+				}
+			}
 		}
 
-		// Return errors if exist
-		if (!empty($errors)) {
-			return $errors;
+		unset($post['password']);
+		unset($post['repeat_password']);
+
+		$result['errors'] = $errors;
+		$result['post'] = $post;
+
+		return $result;
+	}
+
+	public static function register($post) {
+
+		// Validate profile fields
+		$validate = self::validateFields($post);
+		if (!empty($validate['errors'])) {
+			return ['errors' => $validate['errors']];
 		}
 
+		// Update variable after validations (needs for password hash)
+		$post = $validate['post'];
 		// Set client `active`
 		$post['active'] = 1;
 
@@ -69,8 +105,37 @@ class ModuleClients implements IModule {
 		$new_client->loadDataFromArray($post);
 		$new_client->save();
 
-		return true;
+		return ['result' => true];
 
+	}
+
+	public static function saveProfile($post) {
+
+		// Check for client ID
+		$id = (!empty($post['id']) ? $post['id'] : 0);
+		$client = new ClientEntity($id);
+
+		// Stop session and go to login page
+		if (!$client) {
+			ModuleSessions::stop();
+			go(Structure::getPathByLabel('login'));
+		}
+
+		// Validate profile fields
+		$validate = self::validateFields($post, false);
+		if (!empty($validate['errors'])) {
+			return ['errors' => $validate['errors']];
+		}
+
+		// Update variable after validations (needs for password hash)
+		$post = $validate['post'];
+		unset($post['id']);
+
+		// Save
+		$client->loadDataFromArray($post);
+		$client->save();
+
+		return ['result' => true];
 	}
 
 }
